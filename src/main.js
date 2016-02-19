2db44346164fdeb3c78656ccdde810b4bc4a1c06
Zhongyi Tong <zhongyi.tong@foxmail.com>
2016-02-19 19:33:39 +0800

    Prepare for multi session logins, however not supported by electron.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const globalShortcut = electron.globalShortcut;
+const session = require('electron').session;
 
-let mainWindow;
+let windows = {};
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
+let createWindow = () => {
+  let name = (new Date()).getTime();
+
+  windows[name] = new BrowserWindow({
     title: 'WeChat',
     width: 800,
     height: 600,
@@ -15,31 +19,38 @@ function createWindow () {
     center: true,
     show: true,
     frame: true,
-    icon: 'icon.png'
+    icon: 'icon.png',
   });
+  // windows[name].webContents.openDevTools();
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  windows[name].loadURL('file://' + __dirname + '/index.html');
 
-  mainWindow.webContents.on('will-navigate', ev => {
+  windows[name].webContents.on('will-navigate', (ev) => {
     ev.preventDefault();
   });
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-    app.quit();
+  windows[name].on('closed', ()=> {
+    windows[name] = null;
   });
+
+  // globalShortcut.register('CommandOrControl+N', () => {
+  //   createWindow();
+  // });
+
+  // let ses = windows[name].webContents.session;
+  // ses.cookies.remove("https://wx.qq.com", "wxuin", ()=>{})
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
+app.on('activate', () => {
+  if (Object.keys(windows).length == 0) {
     createWindow();
   }
 });
