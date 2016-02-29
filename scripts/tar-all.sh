9eb663050d1bba6b2f6e0838635db27e98f1e8c2
Zhongyi Tong <zhongyi.tong@foxmail.com>
2016-03-01 00:16:02 +0800

    Introduce travis CI.

---
+++ scripts/tar-all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tar zcvf 'dist/mac-osx.tar.gz' 'dist/Electronic WeChat-darwin-x64'
+tar zcvf 'dist/linux-x64.tar.gz' 'dist/Electronic WeChat-linux-x64'
+tar zcvf 'dist/linux-ia32.tar.gz' 'dist/Electronic WeChat-linux-ia32'
