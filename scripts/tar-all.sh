a11534148bee01ab896d20321ea3dbaaf987a5f6
Zhongyi Tong <zhongyi.tong@foxmail.com>
2016-04-21 22:29:30 +0800

    Fix tar script.

--- scripts/tar-all.sh
+++ scripts/tar-all.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+cd dist
+
 echo 'Start compressing for Mac OS X.'
-tar zcf 'dist/mac-osx.tar.gz' 'dist/Electronic WeChat-darwin-x64'
+tar zcf 'mac-osx.tar.gz' 'Electronic WeChat-darwin-x64'
 echo 'Compressing for Mac OS X succeed.'
 
 echo 'Start compressing for Linux x64.'
-tar zcf 'dist/linux-x64.tar.gz' 'dist/Electronic WeChat-linux-x64'
+tar zcf 'linux-x64.tar.gz' 'electronic-wechat-linux-x64'
 echo 'Compressing for Linux x64 succeed.'
 
 echo 'Start compressing for Linux ia32.'
-tar zcf 'dist/linux-ia32.tar.gz' 'dist/Electronic WeChat-linux-ia32'
+tar zcf 'linux-ia32.tar.gz' 'electronic-wechat-linux-ia32'
 echo 'Compressing for Linux ia32 succeed.'
+
+cd ..
