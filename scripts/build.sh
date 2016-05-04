6b10cbda9af110c1bdfafc35541cde2f2fed5e24
afon <wengxufeng@gmail.com>
2016-05-04 11:18:40 +0800

    fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -33,5 +33,5 @@ fi
 
 if [ $PLATFORM = "darwin" ]; then
     ditto -rsrcFork ./dist/Electronic\ WeChat-darwin-x64/Electronic\ WeChat.app /Applications/Electronic\ WeChat.app
-    echo "$(tput setaf 3)App copied to ~/Applications. You can open Electronic WeChat there or from Spotlight.$(tput sgr0)"
+    echo "$(tput setaf 3)App copied to /Applications. You can open Electronic WeChat there or from Spotlight.$(tput sgr0)"
 fi
