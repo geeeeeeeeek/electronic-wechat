96083302d7856e7eb2e152a46e661c72e61e4801
Zhongyi Tong <zhongyi.tong@foxmail.com>
2016-05-19 17:12:24 +0800

    Fix linux tray icon size. Fix #160.

--- src/windows/controllers/app_tray.js
+++ src/windows/controllers/app_tray.js
@@ -15,7 +15,7 @@ class AppTray {
 
     let image;
     if (process.platform == "linux") {
-      image = nativeImage.createFromPath(path.join(__dirname, '../../../assets/icon.png'));
+      image = nativeImage.createFromPath(path.join(__dirname, '../../../assets/status_bar_linux.png'));
     } else {
       image = nativeImage.createFromPath(path.join(__dirname, '../../../assets/status_bar.png'));
     }
