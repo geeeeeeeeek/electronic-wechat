34512c33acee72697a57a0535d1ac7d35b7b204d
ripples <564311873@qq.com>
2016-05-29 17:13:18 +0800

    feat(deploy): update scripts for deploying binaries on qiniu

--- scripts/qiniu.sh
+++ scripts/qiniu.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz -P /home/travis/build/geeeeeeeeek/electronic-wechat
-tar -xvf /home/travis/build/geeeeeeeeek/electronic-wechat/qiniu-devtools-linux_amd64-current.tar.gz -C /home/travis/build/geeeeeeeeek/electronic-wechat
-mkdir /home/travis/build/geeeeeeeeek/electronic-wechat/output
-cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/mac-osx.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
-cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-x64.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
-cp /home/travis/build/geeeeeeeeek/electronic-wechat/dist/linux-ia32.tar.gz /home/travis/build/geeeeeeeeek/electronic-wechat/output
-./qrsync conf.json
+case "$(uname -s)" in
+
+    Linux*)
+        wget http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz -O dist/qiniu-devtools.tar.gz
+        ;;
+
+    Darwin)
+        wget http://devtools.qiniu.io/qiniu-devtools-darwin_amd64-current.tar.gz -O dist/qiniu-devtools.tar.gz
+        ;;
+
+    *)
+        ;;
+
+esac
+
+mkdir dist/qiniu-devtools
+tar -xvf dist/qiniu-devtools.tar.gz -C dist/qiniu-devtools
+rm -rf /tmp/qiniu
+mkdir /tmp/qiniu
+cp dist/mac-osx.tar.gz /tmp/qiniu
+cp dist/linux-x64.tar.gz /tmp/qiniu
+cp dist/linux-ia32.tar.gz /tmp/qiniu
+
+echo '{
+    "src": "/tmp/qiniu",
+    "dest": "qiniu:access_key='$QINIU_ACCESS_KEY'&secret_key='$QINIU_SECRET_KEY'&bucket=flymeos-cancro",
+    "debug_level": 1
+}' > qiniu-config.json
+./dist/qiniu-devtools/qrsync qiniu-config.json
