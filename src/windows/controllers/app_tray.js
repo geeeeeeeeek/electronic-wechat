1d73ae5064c357b5d5c8dd5ae07c5187108a0663
Rocket1184 <rocket1184@outlook.com>
2016-09-16 22:25:59 +0800

    custom tray icon color on linux

--- src/windows/controllers/app_tray.js
+++ src/windows/controllers/app_tray.js
@@ -3,6 +3,7 @@
  */
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 const { app, Menu, nativeImage, Tray } = require('electron');
 
@@ -12,10 +13,23 @@ class AppTray {
   constructor(splashWindow, wechatWindow) {
     this.splashWindow = splashWindow;
     this.wechatWindow = wechatWindow;
+    this.TRAY_CONFIG_PATH = path.join(app.getPath('appData'),'electronic-wechat/trayConfig.json');
 
+    fs.readFile(this.TRAY_CONFIG_PATH, (err, data) => {
+      if(err) {
+        this.trayColor = 'white';
+        fs.writeFile(this.TRAY_CONFIG_PATH, '{"color":"white"}');
+      } else {
+        this.trayColor = JSON.parse(data.toString()).color;
+      }
+      this.creatTray();
+    });
+  }
+
+  creatTray() {
     let image;
     if (process.platform === 'linux') {
-      image = nativeImage.createFromPath(path.join(__dirname, '../../../assets/status_bar_linux.png'));
+      image = nativeImage.createFromPath(path.join(__dirname, `../../../assets/tray_${this.trayColor}.png`));
     } else {
       image = nativeImage.createFromPath(path.join(__dirname, '../../../assets/status_bar.png'));
     }
@@ -25,9 +39,10 @@ class AppTray {
     this.tray.setToolTip(Common.ELECTRONIC_WECHAT);
 
     if (process.platform === 'linux') {
-      const contextMenu = Menu.buildFromTemplate([
+      let contextMenu = Menu.buildFromTemplate([
+        { label: 'ChangeIconColor', click: () => this.changeIconColor() },
         { label: 'Show', click: () => this.hideSplashAndShowWeChat() },
-        { label: 'Exit', click: () => app.exit(0) },
+        { label: 'Exit', click: () => app.exit(0) }
       ]);
       this.tray.setContextMenu(contextMenu);
     }
@@ -41,6 +56,17 @@ class AppTray {
   hideSplashAndShowWeChat() {
     if (this.splashWindow.isShown) return;
     this.wechatWindow.show();
+  }
+
+  changeIconColor() {
+    if(this.trayColor == 'white') {
+      this.trayColor = 'black';
+    } else if (this.trayColor == 'black') {
+      this.trayColor = 'white';
+    }
+    let image = nativeImage.createFromPath(path.join(__dirname, `../../../assets/tray_${this.trayColor}.png`));
+    this.tray.setImage(image);
+    fs.writeFile(this.TRAY_CONFIG_PATH, `{"color":"${this.trayColor}"}`);
   }
 }
 
