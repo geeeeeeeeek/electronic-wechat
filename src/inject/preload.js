07a09e18b5420ec23acd0956ed537c23c8db38ef
Kulbear <yangji0311@gmail.com>
2017-02-12 20:44:30 +0800

    Temporily disable emojione

--- src/inject/preload.js
+++ src/inject/preload.js
@@ -5,8 +5,6 @@ const ShareMenu = require('./share_menu');
 const MentionMenu = require('./mention_menu');
 const BadgeCount = require('./badge_count');
 const Common = require('../common');
-const EmojiParser = require('./emoji_parser');
-const emojione = require('emojione');
 
 const AppConfig = require('../configuration');
 
@@ -97,9 +95,6 @@ class Injector {
     if (!(value.AddMsgList instanceof Array)) return value;
     value.AddMsgList.forEach((msg) => {
       switch (msg.MsgType) {
-        case constants.MSGTYPE_TEXT:
-          msg.Content = EmojiParser.emojiToImage(msg.Content);
-          break;
         case constants.MSGTYPE_EMOTICON:
           Injector.lock(msg, 'MMDigest', '[Emoticon]');
           Injector.lock(msg, 'MsgType', constants.MSGTYPE_EMOTICON);
